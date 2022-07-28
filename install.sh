8368eb120a45c4fa1deec8b17043a3660bac3ebc
rej696 <55083730+rej696@users.noreply.github.com>
2022-07-28 12:55:18 +0100

    create install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+# Neovim
+ln -s /home/$USER/dotfiles/nvim /home/$USER/.config/nvim
+
+# Tmux
+ln -s /home/$USER/dotfiles/.tmux.conf /home/$USER/.tmux.conf
+ln -s /home/$USER/dotfiles/tmux /home/$USER/.tmux
+
