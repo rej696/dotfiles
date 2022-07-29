1c8dd87a8cb0abc79e8eb843b36a2502538b1775
rej696 <55083730+rej696@users.noreply.github.com>
2022-07-29 18:55:18 +0100

    add alacritty, bspwm, rofi and sxhkd config files

--- install.sh
+++ install.sh
@@ -1,7 +1,28 @@
+mkdir -p /home/$USER/.config/
+
 # Neovim
+rm /home/$USER/.config/nvim
 ln -s /home/$USER/dotfiles/nvim /home/$USER/.config/nvim
+
+# Window Manager bspwm
+rm /home/$USER/.config/bspwm
+ln -s /home/$USER/dotfiles/bspwm /home/$USER/.config/bspwm
+rm /home/$USER/.config/sxhkd
+ln -s /home/$USER/dotfiles/sxhkd /home/$USER/.config/sxhkd
+rm /home/$USER/.config/rofi
+ln -s /home/$USER/dotfiles/rofi /home/$USER/.config/rofi
+
+# Alacritty
+rm /home/$USER/.config/alacritty
+ln -s /home/$USER/dotfiles/alacritty /home/$USER/.config/alacritty
 
 # Tmux
 ln -s /home/$USER/dotfiles/.tmux.conf /home/$USER/.tmux.conf
+rm /home/$USER/.tmux
 ln -s /home/$USER/dotfiles/tmux /home/$USER/.tmux
+tmux source-file /home/$USER/.tmux.conf
 
+# Bash
+mv ~/.bashrc ~/.bashrc.bak
+ln -s /home/$USER/dotfiles/.bashrc /home/$USER/.bashrc
+source /home/$USER/.bashrc
