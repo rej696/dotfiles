5ff226cb1bcebbbe79f6b6ed66d57775003feefc
rej696 <55083730+rej696@users.noreply.github.com>
2023-03-08 23:15:58 +0000

    update bspwm keymaps

--- nvim.lazy/lua/config/options.lua
+++ nvim.lazy/lua/config/options.lua
@@ -61,7 +61,7 @@ if vim.g.neovide then
     vim.g.neovide_scroll_animation_length = 0.3
     vim.g.neovide_cursor_animation_length = 0
     vim.g.neovide_cursor_trail_size = 0
-    vim.g.neovide_fullscreen = true
+    -- vim.g.neovide_fullscreen = true
     vim.g.neovide_scale_factor = 1.0
 end
 
