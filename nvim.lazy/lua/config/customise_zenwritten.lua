5f75a1bce328b62f46f798dc9f7c7ca1d942822c
rej696 <55083730+rej696@users.noreply.github.com>
2023-11-16 17:25:30 +0000

    extend zenwritten nvim colorscheme

--- nvim.lazy/lua/config/customise_zenwritten.lua
+++ nvim.lazy/lua/config/customise_zenwritten.lua
@@ -1,9 +1,15 @@
 local lush = require "lush"
 local base = require "zenwritten"
+local p = require "zenwritten.palette"
 
 local specs = lush.parse(function()
     return {
-        Function { base.Function, gui = "italic" },
+        Function { base.Function, gui = "italic", fg = p.dark.leaf},
+        Constant { base.Constant, gui = "italic", fg = p.dark.wood},
+        Preproc { base.Preproc, fg = p.dark.fg },
+        Statement { base.Statement, fg = p.dark.rose},
+        Number { base.Number, fg = p.dark.blossom},
+        Type { base.Type, fg = p.dark.sky},
     }
 end)
 
